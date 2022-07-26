8f65d8e7d086ae4ff9ec774ea4f4cafee06f25b8
francis <francisiyoua05@gmail.com>
2022-07-26 02:10:18 +0200

    ajout de footer.php

---
+++ footer.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Footer Design</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/style_footer.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+<body>
+
+  <footer class="footer">
+  	 <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>My Conciergerie</h4>
+  	 			<ul>
+  	 				<li><a href="#">A Propos</a></li>
+  	 				<li><a href="#">Contact</a></li>
+  	 				<li><a href="#">Politique Privée</a></li>
+  	 				<li><a href="#">Programme Affiliation</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Aide</h4>
+  	 			<ul>
+  	 				<li><a href="#">FAQ</a></li>
+  	 				<li><a href="#">shipping</a></li>
+  	 				<li><a href="#">returns</a></li>
+  	 				<li><a href="#">Statut Juridique</a></li>
+  	 				<li><a href="#">Option de payement</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		
+  	 		<div class="footer-col">
+  	 			<h4>Nous Suivres</h4>
+  	 			<div class="social-links">
+  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i class="fab fa-twitter"></i></a>
+  	 				<a href="#"><i class="fab fa-instagram"></i></a>
+  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div>
+	   <p>&copy; COPYRIGHT, Tout droit reserves 2022 </p>
+  </footer>
+
+</body>
+</html>
